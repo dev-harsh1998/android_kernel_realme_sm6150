9630dc0a9bf4dd71395dbec62830fcbe34c8e22b
dev-harsh1998 <dev-harsh1998@hotmail.com>
2020-04-12 12:57:20 +0530

    security: pfe: import realme code changes

--- security/pfe/pfk.c
+++ security/pfe/pfk.c
@@ -197,6 +197,8 @@ static inline bool pfk_is_ready(void)
  */
 static struct inode *pfk_bio_get_inode(const struct bio *bio)
 {
+	struct address_space *mapping = NULL;
+
 	if (!bio)
 		return NULL;
 	if (!bio_has_data((struct bio *)bio))
@@ -216,10 +218,11 @@ static struct inode *pfk_bio_get_inode(const struct bio *bio)
 		return inode;
 	}
 
-	if (!page_mapping(bio->bi_io_vec->bv_page))
+	mapping = page_mapping(bio->bi_io_vec->bv_page);
+	if (!mapping)
 		return NULL;
 
-	return page_mapping(bio->bi_io_vec->bv_page)->host;
+	return mapping->host;
 }
 
 /**

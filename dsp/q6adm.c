77642dd14877def7e3d73247fef71a1044aefd14
Harshit Jain <dev-harsh1998@hotmail.com>
2022-02-07 00:41:34 +0530

    dsp: q6adm: add back support for ADM_AUDPROC_PERSISTENT_CAL_TYPE
    * Qcom dropped the support for aforementioned cal in commit dev-harsh1998/techpack_x2@b4f1c986d26ebfb0bf8840189a922a3a77d1261b however realme had plans to use
    ancient dsp fw that advertises support for that cal type and requires the ADM calibration to be persisted in memory with size smaller than (2 * 1024 * 1020)
    introduced in commit dev-harsh1998/techpack_x2@bbb641427bccc09884da3f458b2c2bd489047c65 but since both conditions were unmet this ended up breaking the mic
    during incall, So add the cal_type back to satisfy the dsp stack and bring back mic.
    
    * Based on X2-pro/X3 changes + Debugging over the following log
    
    [  306.505654] (2)[3236:writer]q6asm_send_cal: cal_block is NULL
    [  306.780696] (1)[3783:EventBus0]qdsp_cvp_callback: cmd = 0x1307c returned error = 0x9
    [  306.780734] (4)[787:HwBinder:751_2]voice_send_cvp_deregister_vol_cal_cmd: DSP returned error[ADSP_EALREADY]
    [  306.780926] (1)[3783:EventBus0]qdsp_cvp_callback: cmd = 0x1307a returned error = 0x9
    [  306.781076] (4)[787:HwBinder:751_2]voice_send_cvp_deregister_cal_cmd: DSP returned error[ADSP_EALREADY]
    [  306.781264] (1)[1336:Binder:1303_2]qdsp_cvp_callback: cmd = 0x11372 returned error = 0xa
    [  306.781309] (4)[787:HwBinder:751_2]voice_send_cvp_deregister_dev_cfg_cmd: DSP returned error[ADSP_ENOTREADY]
    [  306.781498] (1)[1336:Binder:1303_2]qdsp_cvs_callback: cmd = 0x1307e returned error = 0x9
    [  306.782106] (0)[787:HwBinder:751_2]voice_send_cvs_deregister_cal_cmd: DSP returned error[ADSP_EALREADY]
    [  306.784697] (0)[787:HwBinder:751_2]voice_unmap_cal_block: Cal block is NULL, index 2!
    [  306.784702] (0)[787:HwBinder:751_2]voice_destroy_mvm_cvs_session: Unmap VOCPROC cal failed
    [  306.784705] (0)[787:HwBinder:751_2]voice_unmap_cal_block: Cal block is NULL, index 3!
    [  306.784707] (0)[787:HwBinder:751_2]voice_destroy_mvm_cvs_session: Unmap VOCVOL cal failed
    [  306.784709] (0)[787:HwBinder:751_2]voice_unmap_cal_block: Cal block is NULL, index 4!
    [  306.784711] (0)[787:HwBinder:751_2]voice_destroy_mvm_cvs_session: Unmap VOCDEV_CFG cal failed
    [  306.784713] (0)[787:HwBinder:751_2]voice_unmap_cal_block: Cal block is NULL, index 7!
    [  306.784715] (0)[787:HwBinder:751_2]voice_destroy_mvm_cvs_session: Unmap VOCSTRM cal failed
    [  306.800910] (6)[106:kworker/6:1] msm_dmic_event: dmic_switch_gpio invalid
    
    Signed-off-by: Harshit Jain <dev-harsh1998@hotmail.com>

--- dsp/q6adm.c
+++ dsp/q6adm.c
@@ -2177,7 +2177,8 @@ static struct cal_block_data *adm_find_cal_by_path(int cal_index, int path)
 
 		if (cal_index == ADM_AUDPROC_CAL ||
 		    cal_index == ADM_LSM_AUDPROC_CAL ||
-		    cal_index == ADM_LSM_AUDPROC_PERSISTENT_CAL) {
+		    cal_index == ADM_LSM_AUDPROC_PERSISTENT_CAL ||
+			cal_index == ADM_AUDPROC_PERSISTENT_CAL) {
 			audproc_cal_info = cal_block->cal_info;
 			if ((audproc_cal_info->path == path) &&
 			    (cal_block->cal_data.size > 0))
@@ -2215,7 +2216,8 @@ static struct cal_block_data *adm_find_cal_by_app_type(int cal_index, int path,
 
 		if (cal_index == ADM_AUDPROC_CAL ||
 		    cal_index == ADM_LSM_AUDPROC_CAL ||
-		    cal_index == ADM_LSM_AUDPROC_PERSISTENT_CAL) {
+		    cal_index == ADM_LSM_AUDPROC_PERSISTENT_CAL ||
+			cal_index == ADM_AUDPROC_PERSISTENT_CAL) {
 			audproc_cal_info = cal_block->cal_info;
 			if ((audproc_cal_info->path == path) &&
 			    (audproc_cal_info->app_type == app_type) &&
@@ -2256,7 +2258,8 @@ static struct cal_block_data *adm_find_cal(int cal_index, int path,
 
 		if (cal_index == ADM_AUDPROC_CAL ||
 		    cal_index == ADM_LSM_AUDPROC_CAL ||
-		    cal_index == ADM_LSM_AUDPROC_PERSISTENT_CAL) {
+		    cal_index == ADM_LSM_AUDPROC_PERSISTENT_CAL ||
+			cal_index == ADM_AUDPROC_PERSISTENT_CAL) {
 			audproc_cal_info = cal_block->cal_info;
 			if ((audproc_cal_info->path == path) &&
 			    (audproc_cal_info->app_type == app_type) &&
@@ -2347,6 +2350,9 @@ static void send_adm_cal(int port_id, int copp_idx, int path, int perf_mode,
 	if (passthr_mode != LISTEN) {
 		send_adm_cal_type(ADM_AUDPROC_CAL, path, port_id, copp_idx,
 				perf_mode, app_type, acdb_id, sample_rate);
+		send_adm_cal_type(ADM_AUDPROC_PERSISTENT_CAL, path,
+				  port_id, copp_idx, perf_mode, app_type,
+				  acdb_id, sample_rate);
 	} else {
 		send_adm_cal_type(ADM_LSM_AUDPROC_CAL, path, port_id, copp_idx,
 				  perf_mode, app_type, acdb_id, sample_rate);
@@ -3901,6 +3907,9 @@ static int get_cal_type_index(int32_t cal_type)
 	case ADM_LSM_AUDPROC_PERSISTENT_CAL_TYPE:
 		ret = ADM_LSM_AUDPROC_PERSISTENT_CAL;
 		break;
+	case ADM_AUDPROC_PERSISTENT_CAL_TYPE:
+		ret = ADM_AUDPROC_PERSISTENT_CAL;
+		break;
 	default:
 		pr_err("%s: invalid cal type %d!\n", __func__, cal_type);
 	}
@@ -4124,6 +4133,12 @@ static int adm_init_cal_data(void)
 		cal_utils_match_buf_num} },
 
 		{{ADM_LSM_AUDPROC_PERSISTENT_CAL_TYPE,
+		 {adm_alloc_cal, adm_dealloc_cal, NULL,
+		  adm_set_cal, NULL, NULL} },
+		 {adm_map_cal_data, adm_unmap_cal_data,
+		  cal_utils_match_buf_num} },
+
+		{{ADM_AUDPROC_PERSISTENT_CAL_TYPE,
 		 {adm_alloc_cal, adm_dealloc_cal, NULL,
 		  adm_set_cal, NULL, NULL} },
 		 {adm_map_cal_data, adm_unmap_cal_data,
@@ -4640,6 +4655,13 @@ int adm_store_cal_data(int port_id, int copp_idx, int path, int perf_mode,
 			goto unlock;
 		}
 	} else if (cal_index == ADM_LSM_AUDPROC_PERSISTENT_CAL) {
+		if (cal_block->cal_data.size > AUD_PROC_PERSIST_BLOCK_SIZE) {
+			pr_err("%s:persist invalid size exp/actual[%zd, %d]\n",
+				__func__, cal_block->cal_data.size, *size);
+			rc = -ENOMEM;
+			goto unlock;
+		}
+	} else if (cal_index == ADM_AUDPROC_PERSISTENT_CAL) {
 		if (cal_block->cal_data.size > AUD_PROC_PERSIST_BLOCK_SIZE) {
 			pr_err("%s:persist invalid size exp/actual[%zd, %d]\n",
 				__func__, cal_block->cal_data.size, *size);
